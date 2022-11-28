f8c17be262d3acef90f91ce9a9e067a758b7f473
jjpaquin <79430973+jjpaquin@users.noreply.github.com>
2022-11-27 19:59:50 -0500

    loginpagesv1

--- connect-db.php
+++ connect-db.php
@@ -1,100 +1,19 @@
 <?php
-// Remember to start the database server (or GCP SQL instance) before trying to connect to it
-////////////////////////////////////////////
-/** F22, PHP (on Google Standard App Engine) connect to MySQL instance (GCP) **/
-// $username = 'root';                       // or your username
-// $password = 'your-root-password';     
-// $host = 'instance-connection-name';       // e.g., 'cs4750:us-east4:db-demo'; 
-// $dbname = 'your-database-name';           // e.g., 'guestbook';
-// $dsn = "mysql:unix_socket=/cloudsql/instance-connection-name;dbname=your-database-name";
-//       e.g., "mysql:unix_socket=/cloudsql/cs4750:us-east4:db-demo;dbname=guestbook";
 
-// to get instance connection name, go to GCP SQL overview page
-////////////////////////////////////////////
+$sname= "localhost:3306";
 
-/** F22, PHP (on local XAMPP or CS server) connect to MySQL instance (GCP) **/
-// $username = 'root';
-// $password = 'your-root-password';
-// $host = 'instance-connection-name';       // e.g., 'cs4750:us-east4:db-demo'; 
-// $dbname = 'your-database-name;;           // e.g., 'guestbook';
-// $dsn = "mysql:host=your-SQL-public-IP-address;dbname=your-database-name";   // connect PHP (XAMPP) to DB (GCP)
-//       e.g., "mysql:host=99.99.999.99;dbname=$dbname";   
+$unmae= "joseph";
 
-// to get public IP addres of the SQL instance, go to GCP SQL overview page
+$pass = "josephDBpass";
 
-// To connect from a local PHP to GCP SQL instance, need to add authormized network
-// to allow (my)machine to connect to the SQL instance. 
-// 1. Get IP of the computer that tries to connect to the SQL instance
-//    (use http://ipv4.whatismyv6.com/ to find the IP address)
-// 2. On the SQL connections page, add authorized networks, enter the IP address
-////////////////////////////////////////////
+$db_name = "statify2";
 
-/** F22, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on local XAMPP) **/
-$username = 'ethan_a';
-$password = 'bayhawks79';
-$host = 'localhost:3306';
-$dbname = 'cs46750-FriendBook';
-$dsn = "mysql:host=$host;dbname=$dbname";  
-////////////////////////////////////////////
+$conn = mysqli_connect($sname, $unmae, $pass, $db_name);
 
-/** F22, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on CS server) **/
-// $username = 'your-computingID'; 
-// $password = 'your-mysql-password';
-// $host = 'mysql01.cs.virginia.edu';
-// $dbname = 'your-computingID';
-// $dsn = "mysql:host=$host;dbname=$dbname";
+if (!$conn) {
 
-////////////////////////////////////////////
+    echo "Connection failed!";
 
-// To find a hostname, access phpMyAdmin
-// - select tob "User accounts"
-// - locate the username you created, by default, the Host name is localhost
-
-// To find a port number, access phpMyAdmin
-// - use Console (bottom)
-// - type     SHOW VARIABLES WHERE Variable_name = 'port';
-// - execute the query    press Ctrl+Enter
-// (default port to mySQL database in XAMPP is 3306)
-
-// Be sure to use the correct database name (also case-sensitive)
-//   Note: Looking in the wrong database and/or wrong table may results in either
-//         cannot connect to the database, not find table, or no result set.
-//         Thus, specify the correct database name
-
-
-// DSN (Data Source Name) specifies the host computer for the MySQL datbase 
-// and the name of the database. If the MySQL datbase is running on the same server
-// as PHP, use the localhost keyword to specify the host computer
-
-// To connect to a MySQL database named db-demo, need three arguments: 
-// - specify a DSN, username, and password
-
-// Create an instance of PDO (PHP Data Objects) which connects to a MySQL database
-// (PDO defines an interface for accessing databases)
-// Syntax: 
-//    new PDO(dsn, username, password);
-
-
-/** connect to the database **/
-try 
-{
-//  $db = new PDO("mysql:host=$hostname;dbname=db-demo", $username, $password);
-   $db = new PDO($dsn, $username, $password);
-   
-   // display a message to let us know that we are connected to the database 
-   // echo "<p>You are connected to the database</p>";
-}
-catch (PDOException $e)     // handle a PDO exception (errors thrown by the PDO library)
-{
-   // Call a method from any object, use the object's name followed by -> and then method's name
-   // All exception objects provide a getMessage() method that returns the error message 
-   $error_message = $e->getMessage();        
-   echo "<p>An error occurred while connecting to the database: $error_message </p>";
-}
-catch (Exception $e)       // handle any type of exception
-{
-   $error_message = $e->getMessage();
-   echo "<p>Error message: $error_message </p>";
 }
 
 ?>
---
+++ login.php
@@ -0,0 +1,52 @@
+<?php
+session_start();
+include "connect-db.php";
+
+if(isset($_POST['username']) && isset($_POST['password'])) {
+
+    function validate($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+$username = validate($_POST['username']);
+$password = validate($_POST['password']);
+
+if(empty($username)) {
+    header ("Location: index.php?error=Username is required");
+    exit();
+}
+
+if(empty($password)) {
+    header ("Location: index.php?error=Password is required");
+    exit();
+}
+
+$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+
+$result = mysqli_query($conn, $sql);
+
+if(mysqli_num_rows($result) === 1) {
+    $row = mysqli_fetch_assoc($result);
+    if($row['username'] === $username && $row['password'] === $password) {
+        echo "Logged In";
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['name'] = $row['first_name'];
+        $_SESSION['id'] = $row['userID'];
+        header("Location: home.php");
+        exit();
+    }
+    else{
+        header("Location: index.php?error=Incorrect Username or Password");
+        exit();
+    }
+}
+else {
+    header("Location: index.php?error=Incorrect Username or Password");
+    exit();
+}
+
+?>
