a5be6f660ca9d71f0b8b6771f850df13e9fa3785
Ethan <eashley7901@gmail.com>
2022-11-29 18:25:20 -0500

    added average stats

--- statistics.php
+++ statistics.php
@@ -2,25 +2,36 @@
     require("connect-db.php");
 
     $album_sql = "SELECT * FROM `album`";
-    $albums = $conn->query($album_sql);
+    $albums = $con->query($album_sql);
 
     $artist_sql = "SELECT * FROM `artist`";
-    $artists = $conn->query($artist_sql);
+    $artists = $con->query($artist_sql);
 
     $playlist_sql = "SELECT * FROM `playlist`";
-    $playlists = $conn->query($playlist_sql);
+    $playlists = $con->query($playlist_sql);
 
     $stats = NULL;
+    $averagestats = NULL;
 ?>
 
 <?php
     if(isset($_POST['albumstat']))
     {
         $stats = NULL;
-        $albumName = mysqli_real_escape_string($conn,$_POST['Album']);
+        $albumName = mysqli_real_escape_string($con,$_POST['Album']);
         $album_stat_sql = "SELECT name, duration, instrumentalness, loudness, danceability, tempo, energy, song_key, valence
          FROM `album` NATURAL JOIN `part_of` NATURAL JOIN `song` WHERE album_name = '$albumName'";
-        $stats = $conn->query($album_stat_sql);   
+        $stats = $con->query($album_stat_sql);
+    }
+
+    if(isset($_POST['albumavg']))
+    {
+        $stats = NULL;
+        $albumName = mysqli_real_escape_string($con,$_POST['Album']);
+        $album_avg_sql = "SELECT AVG(duration) as duration, AVG(instrumentalness) as instrumentalness, AVG(loudness) as loudness, 
+         AVG(danceability) as danceability, AVG(tempo) as tempo, AVG(energy) as energy, AVG(song_key) as song_key, AVG(valence) as valence
+         FROM `album` NATURAL JOIN `part_of` NATURAL JOIN `song` WHERE album_name = '$albumName'";
+        $stats = $con->query($album_avg_sql);  
     }
 ?>
 
@@ -28,11 +39,22 @@
     if(isset($_POST['artiststat']))
     {
         $stats = NULL;
-        $artistName = mysqli_real_escape_string($conn,$_POST['Artist']);
+        $artistName = mysqli_real_escape_string($con,$_POST['Artist']);
         $artist_stat_sql = "SELECT name, duration, instrumentalness, loudness, danceability, tempo, energy, song_key, valence
          FROM `artist` NATURAL JOIN `made` NATURAL JOIN `album` NATURAL JOIN `part_of` NATURAL JOIN `song` 
          WHERE artist_name = '$artistName'";
-        $stats = $conn->query($artist_stat_sql);   
+        $stats = $con->query($artist_stat_sql);   
+    }
+
+    if(isset($_POST['artistavg']))
+    {
+        $stats = NULL;
+        $artistName = mysqli_real_escape_string($con,$_POST['Artist']);
+        $artist_avg_sql = "SELECT AVG(duration) as duration, AVG(instrumentalness) as instrumentalness, AVG(loudness) as loudness, 
+         AVG(danceability) as danceability, AVG(tempo) as tempo, AVG(energy) as energy, AVG(song_key) as song_key, AVG(valence) as valence
+         FROM `artist` NATURAL JOIN `made` NATURAL JOIN `album` NATURAL JOIN `part_of` NATURAL JOIN `song` 
+         WHERE artist_name = '$artistName'";
+        $stats = $con->query($artist_avg_sql);  
     }
 ?>
 
@@ -40,10 +62,25 @@
     if(isset($_POST['playliststat']))
     {
         $stats = NULL;
-        $playlistName = mysqli_real_escape_string($conn,$_POST['Playlist']);
+        $playlistName = mysqli_real_escape_string($con,$_POST['Playlist']);
         $playlist_stat_sql = "SELECT name, duration, instrumentalness, loudness, danceability, tempo, energy, song_key, valence
          FROM `playlist` NATURAL JOIN `contains` NATURAL JOIN `song` WHERE playlist_name = '$playlistName'";
-        $stats = $conn->query($playlist_stat_sql);   
+        $stats = $con->query($playlist_stat_sql);   
+
+        $averagestats = NULL;
+        $playlist_avg_sql = "SELECT AVG(duration), AVG(instrumentalness), AVG(loudness), AVG(danceability), AVG(tempo), AVG(energy), AVG(song_key), AVG(valence)
+         FROM `playlist` NATURAL JOIN `contains` NATURAL JOIN `song` WHERE playlist_name = '$playlistName'";
+        $averagestats = $con->query($playlist_avg_sql);
+    }
+
+    if(isset($_POST['playlistavg']))
+    {
+        $stats = NULL;
+        $playlistName = mysqli_real_escape_string($con,$_POST['Playlist']);
+        $playlist_avg_sql = "SELECT AVG(duration) as duration, AVG(instrumentalness) as instrumentalness, AVG(loudness) as loudness, 
+         AVG(danceability) as danceability, AVG(tempo) as tempo, AVG(energy) as energy, AVG(song_key) as song_key, AVG(valence) as valence
+         FROM `playlist` NATURAL JOIN `contains` NATURAL JOIN `song` WHERE playlist_name = '$playlistName'";
+        $stats = $con->query($playlist_avg_sql);  
     }
 ?>
 
@@ -80,7 +117,8 @@
             </option>
         <?php endforeach; ?>
     </select>
-    <input type="submit" value="Submit" name="albumstat" style="width: fit-content;" title="Submit">
+    <input type="submit" value="Individual Statistics" name="albumstat" style="width: fit-content;" title="Submit">
+    <input type="submit" value="Average Statistics" name="albumavg" style="width: fit-content;" title="Submit">
 
     <h3>Get Artist Statistics:</h3>
     Artist:
@@ -94,7 +132,8 @@
             </option>
         <?php endforeach; ?>
     </select>
-    <input type="submit" value="Submit" name="artiststat" style="width: fit-content;" title="Submit">
+    <input type="submit" value="Individual Statistics" name="artiststat" style="width: fit-content;" title="Submit">
+    <input type="submit" value="Average Statistics" name="artistavg" style="width: fit-content;" title="Submit">
 
     <h3>Get Playlist Statistics:</h3>
     Playlist:
@@ -108,16 +147,17 @@
             </option>
         <?php endforeach; ?>
     </select>
-    <input type="submit" value="Submit" name="playliststat" style="width: fit-content;" title="Submit">
+    <input type="submit" value="Individual Statistics" name="playliststat" style="width: fit-content;" title="Submit">
+    <input type="submit" value="Average Statistics" name="playlistavg" style="width: fit-content;" title="Submit">
 </form>
 
 <hr/>
-<h3>Statistics</h3>
+<h3 style="color:white;">Statistics</h3>
 <div class="row justify-content-center">  
-<table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
+<table class="w3-table w3-bordered w3-card-4 center" style="width:100%; background-color:#B0B0B0;">
   <thead>
   <tr style="background-color:#B0B0B0">
-    <th width="11%"><b>Name</b></th>
+    <th width="11%"><b>Song</b></th>
     <th width="11%"><b>Duration</b></th>
     <th width="11%"><b>Instrumentalness</b></th>        
     <th width="11%"><b>Loudness</b></th> 
@@ -130,7 +170,15 @@
   </thead>
     <?php foreach ($stats as $song): ?>
         <tr>
-            <td style="text-align:center"><?php echo $song['name']; ?></td>
+            <?php
+                if ($song['name']){
+                    $song_name = $song['name'];
+                }else{
+                    $song_name = "AVERAGE";
+                }
+
+            ?>
+            <td style="text-align:center"><?php echo $song_name; ?></td>
             <td style="text-align:center"><?php echo $song['duration']; ?></td>        
             <td style="text-align:center"><?php echo $song['instrumentalness']; ?></td>
             <td style="text-align:center"><?php echo $song['loudness']; ?></td>
