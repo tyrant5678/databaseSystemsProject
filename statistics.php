7bcf3eba782bcdae3e55cbb07418c55530bd339a
Ethan <eashley7901@gmail.com>
2022-11-29 02:20:56 -0500

    added song statistics

--- statistics.php
+++ statistics.php
@@ -1,33 +1,49 @@
 <?php
     require("connect-db.php");
-    require("stats.php");
+
+    $album_sql = "SELECT * FROM `album`";
+    $albums = $con->query($album_sql);
+
+    $artist_sql = "SELECT * FROM `artist`";
+    $artists = $con->query($artist_sql);
+
+    $playlist_sql = "SELECT * FROM `playlist`";
+    $playlists = $con->query($playlist_sql);
+
+    $stats = NULL;
 ?>
 
 <?php
-    global $conn;
-    $query = "SELECT * FROM album";  
-    try {
-        $statement = $conn->prepare($query);
-        $statement->execute();
-        $result = $statement->fetchAll();   // fetch()
-        $statement->closeCursor();
-        echo $result;
-    }
-    catch (PDOException $e) 
+    if(isset($_POST['albumstat']))
     {
-        echo $e->getMessage();
-        if there is a specific SQL-related error message
-           echo "generic message (don't reveal SQL-specific message)";
-
-        if (str_contains($e->getMessage(), "Duplicate"))
-		   echo "Failed to add a friend <br/>";
-
-        if ($statement->rowCount() == 0)
-            echo "Failed to add a friend <br/>";
+        $stats = NULL;
+        $albumName = mysqli_real_escape_string($con,$_POST['Album']);
+        $album_stat_sql = "SELECT name, duration, instrumentalness, loudness, danceability, tempo, energy, song_key, valence
+         FROM `album` NATURAL JOIN `part_of` NATURAL JOIN `song` WHERE album_name = '$albumName'";
+        $stats = $con->query($album_stat_sql);   
     }
-    catch (Exception $e)
+?>
+
+<?php
+    if(isset($_POST['artiststat']))
     {
-        echo $e->getMessage();
+        $stats = NULL;
+        $artistName = mysqli_real_escape_string($con,$_POST['Artist']);
+        $artist_stat_sql = "SELECT name, duration, instrumentalness, loudness, danceability, tempo, energy, song_key, valence
+         FROM `artist` NATURAL JOIN `made` NATURAL JOIN `album` NATURAL JOIN `part_of` NATURAL JOIN `song` 
+         WHERE artist_name = '$artistName'";
+        $stats = $con->query($artist_stat_sql);   
+    }
+?>
+
+<?php
+    if(isset($_POST['playliststat']))
+    {
+        $stats = NULL;
+        $playlistName = mysqli_real_escape_string($con,$_POST['Playlist']);
+        $playlist_stat_sql = "SELECT name, duration, instrumentalness, loudness, danceability, tempo, energy, song_key, valence
+         FROM `playlist` NATURAL JOIN `contains` NATURAL JOIN `song` WHERE playlist_name = '$playlistName'";
+        $stats = $con->query($playlist_stat_sql);   
     }
 ?>
 
@@ -39,7 +55,7 @@
 	<meta name="viewport"
 		content="width=device-width, initial-scale=1.0">
 
-	<?php include('header.html') ?> 
+	<!-- <?php include('header.html') ?>  -->
     <title> Statistics </title>
     
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -52,41 +68,79 @@
   <h1>Statistics</h1>  
 
 <form name="statisticsForm" action="statistics.php" method="post">   
-
-<h3>Get Average Statistics by:</h3>
+    <h3>Get Album Statistics:</h3>
     Album:
+    <select name="Album">
+        <option value="none">
+            <?php echo "";?>
+        </option>
+        <?php foreach ($albums as $album): ?>
+            <option value="<?php echo $album["album_name"]; ?>">
+                <?php echo $album["album_name"]; ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <input type="submit" value="Submit" name="albumstat" style="width: fit-content;" title="Submit">
 
-    <!-- <?php foreach ($list_of_playlists as $playlist_info): ?>
-    <tr>
-        <td><?php echo $friend_info['name']; ?></td>
-    </tr>
-    <?php endforeach; ?> -->
+    <h3>Get Artist Statistics:</h3>
+    Artist:
+    <select name="Artist">
+        <option value="none">
+            <?php echo "";?>
+        </option>
+        <?php foreach ($artists as $artist): ?>
+            <option value="<?php echo $artist["artist_name"]; ?>">
+                <?php echo $artist["artist_name"]; ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <input type="submit" value="Submit" name="artiststat" style="width: fit-content;" title="Submit">
 
+    <h3>Get Playlist Statistics:</h3>
+    Playlist:
+    <select name="Playlist">
+        <option value="none">
+            <?php echo "";?>
+        </option>
+        <?php foreach ($playlists as $playlist): ?>
+            <option value="<?php echo $playlist["playlist_name"]; ?>">
+                <?php echo $playlist["playlist_name"]; ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <input type="submit" value="Submit" name="playliststat" style="width: fit-content;" title="Submit">
 </form>
 
 <hr/>
-<h3>Average Statistics</h3>
+<h3>Statistics</h3>
 <div class="row justify-content-center">  
 <table class="w3-table w3-bordered w3-card-4 center" style="width:100%">
   <thead>
   <tr style="background-color:#B0B0B0">
-    <th width="12.5%"><b>Duration</b></th>
-    <th width="12.5%"><b>Instrumentalness</b></th>        
-    <th width="12.5%"><b>Loudness</b></th> 
-    <th width="12.5%"><b>Danceability</b></th>
-    <th width="12.5%"><b>Tempo</b></th>
-    <th width="12.5%"><b>Energy</b></th> 
-    <th width="12.5%"><b>Key</b></th>
-    <th width="12.5%"><b>Valence</b></th>
+    <th width="11%"><b>Name</b></th>
+    <th width="11%"><b>Duration</b></th>
+    <th width="11%"><b>Instrumentalness</b></th>        
+    <th width="11%"><b>Loudness</b></th> 
+    <th width="11%"><b>Danceability</b></th>
+    <th width="11%"><b>Tempo</b></th>
+    <th width="11%"><b>Energy</b></th> 
+    <th width="11%"><b>Key</b></th>
+    <th width="11%"><b>Valence</b></th>
   </tr>
   </thead>
-<?php foreach ($list_of_friends as $friend_info): ?>
-  <tr>
-     <td><?php echo $friend_info['name']; ?></td>
-     <td><?php echo $friend_info['major']; ?></td>        
-     <td><?php echo $friend_info['year']; ?></td>
-  </tr>
-<?php endforeach; ?>
+    <?php foreach ($stats as $song): ?>
+        <tr>
+            <td style="text-align:center"><?php echo $song['name']; ?></td>
+            <td style="text-align:center"><?php echo $song['duration']; ?></td>        
+            <td style="text-align:center"><?php echo $song['instrumentalness']; ?></td>
+            <td style="text-align:center"><?php echo $song['loudness']; ?></td>
+            <td style="text-align:center"><?php echo $song['danceability']; ?></td>        
+            <td style="text-align:center"><?php echo $song['tempo']; ?></td>
+            <td style="text-align:center"><?php echo $song['energy']; ?></td>
+            <td style="text-align:center"><?php echo $song['song_key']; ?></td>        
+            <td style="text-align:center"><?php echo $song['valence']; ?></td>
+        </tr>
+    <?php endforeach; ?>
 </table>
 </div>   
      
